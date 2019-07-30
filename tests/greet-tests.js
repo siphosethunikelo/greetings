b82e501b82eebed5a36b3038bd6a93f65b2b56e9
Siphosethu <siphosethunikelo@gmail.com>
2019-07-30 08:24:04 -0400

    done

--- tests/greet-tests.js
+++ tests/greet-tests.js
@@ -22,31 +22,15 @@ describe("greet factory function", function () {
                  assert.equal(4, greetInstance.getCounter());
     
              })
+             it ("this should passed the correct all name greeted",function(){
+                let greetInstance = greetFunc();
+                greetInstance.setName('JaN');
+                greetInstance.setName('JanE');
+                greetInstance.setName('James');
+                greetInstance.setName('John');
+                assert.equal(4, greetInstance.getCounter());
+   
     })
-        // it("this should passed the correct name greeted",function(){
-        //      let greetInstance = greetFunc();
-             
-        //     assert.equal("Molo", greetInstance.xhosaGreet());
-        // })
-        // it("this will return name greeted",function(){
-        //     let greetInstance = greetFunc();
-        //     assert.equal('Goeie moere sbu',greetInstance.afrikaansGreet('sbu'));
-        // })
-     
-        // })
-        // it ("this will be able return all names greeted",function(){
-        //     let greetInstance = greetFunc();
-        //     greetInstance.setName('Jan');
-        //     greetInstance.setName('Jane');
-        //     greetInstance.setName('James');
-        //     greetInstance.setName('John');
-        //     assert.equal(4, greetInstance.getCounter());
-
-        //  })
-
+})
+        
     
-        // it ("this will be able to return all names",function(){
-        //     let greetInstance = greetFunc();
-        //     assert.equal('Molo Zola',greetInstance.xhosaGreet());
-        // })
-    // })
